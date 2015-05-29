7f37d9c4110253b5f70a7fc14b52eac7797059cb
Dima <admin.ptm@gmail.com>
2015-05-29 11:25:01 +0300

    Revert "Revert "Updating""
    This reverts commit 0d41878fa07271a072db882328fefb69c6a37469.

--- JSON.java
+++
@@ -1,5 +0,0 @@
-package jsonYahooFinanceParser;
-
-public class JSON {
-    public Query query;
-}
--- Query.java
+++ Query.java
@@ -1,8 +1,54 @@
-package jsonYahooFinanceParser;
+package dz2.xmlYahooFinanceParser;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "query")
 public class Query {
-    public int count;
-    public String created;
-    public String lang;
-    public Results results;
+	@XmlElement(name = "results")
+	private List<Results> resultss;
+	private int count;
+	private String created;
+	private String lang;
+	
+	public void add(Results results) {
+        resultss.add(results);
+	}
+	
+	@XmlElement(name = "Count")
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	@XmlElement(name = "Created")
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	
+	@XmlElement(name = "Lang")
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public String getLang() {
+		return lang;
+	}
+
+	 @Override
+	 public String toString() {
+	 return Arrays.deepToString(resultss.toArray());
+	 }
+
+
 }
